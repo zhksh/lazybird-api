c0ca3a00b5f86b29340676fe65a98928004cd1b8
Chris on Mac <20260809+ChSchmidt99@users.noreply.github.com>
2022-11-20 20:51:21 +0100

    added .env file

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+
 const app = express()
-const PORT = 6969
+
+function getPort(): number {
+    const port = Number(process.env.API_PORT)
+    if(port) {
+        return port
+    }
+
+    // Default port, if no other port is specified in env.
+    return 6969
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.listen(PORT, () => console.log(`api running on http://localhost:${PORT}`))
+app.listen(getPort(), () => console.log(`api running on http://localhost:${getPort()}`))
