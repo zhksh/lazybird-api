c56d4859bd1d985b7ed4cc63c4b83913507ebc28
Pascal Guldener <pascal@guldener.io>
2022-12-05 22:26:33 +0100

    some comments

--- src/service/config.ts
+++ src/service/config.ts
@@ -1,3 +1,4 @@
+// do not change
 export const BACKEND_HOST = 'http:/138.246.237.14/'
 // export const BACKEND_HOST = 'http:/localhost:5000/'
 export const IN_CONTEXT_PATH = 'api/create-incontext-post'
