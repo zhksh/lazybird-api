c068f570ac33d84103789eb505a9818adf65c648
Pascal Guldener <pascal@guldener.io>
2022-12-05 19:58:22 +0100

    dokcer dont read hosts file, so use ip

--- src/service/config.ts
+++ src/service/config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_HOST = 'http://lmb.io/'
+export const BACKEND_HOST = 'http:/138.246.237.14/'
 // export const BACKEND_HOST = 'http:/localhost:5000/'
 export const IN_CONTEXT_PATH = 'api/create-incontext-post'
 export const AUTOCOMPLETE_PATH = 'api/complete-post'
