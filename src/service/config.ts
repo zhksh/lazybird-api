2b2751b78169eff5e5dd17d6b78ceaa0320afeda
Pascal Guldener <pascal@guldener.io>
2022-12-06 14:27:59 +0100

    changed conf from local to prod

--- src/service/config.ts
+++ src/service/config.ts
@@ -1,6 +1,6 @@
 // do not change
-// export const BACKEND_HOST = 'http:/138.246.237.14/'
-export const BACKEND_HOST = 'http:/localhost:5000/'
+export const BACKEND_HOST = 'http:/138.246.237.14/'
+// export const BACKEND_HOST = 'http:/localhost:5000/'
 export const IN_CONTEXT_PATH = 'api/create-incontext-post'
 export const AUTOCOMPLETE_PATH = 'api/complete-post'
 export const TIMEOUT = 60000 //ms
