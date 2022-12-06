62c2c6cb0035eeba69d7a287da630ffcb314fb67
Pascal Guldener <pascal@guldener.io>
2022-12-06 14:09:20 +0100

    some errhandling

--- src/api/backendRoutes.ts
+++ src/api/backendRoutes.ts
@@ -1,4 +1,3 @@
-import { Pool } from 'pg'
 import express from 'express'
 import { HTTP_INTERNAL_ERROR, HTTP_SUCCESS } from './codes';
 import {complete, createInContextPost} from "../service/postGeneraton";
@@ -12,43 +11,28 @@ backendRouter.post('/incontext', async function(req, res) {
   // const userId:string = req.body.user
   // const postId:string = req.body.postId
 
-    const context = JSON.parse(JSON.stringify({
-      "context" : [
-        {
-          "input": "Hi Dude",
-          "output": "Hi man, what are you up to ?",
-          "ts": 123918273,
-          "source": "lm"
-        },
-        {
-          "input": "Not much, working mostly.",
-          "output": "Thats too bad im in Malaga !",
-          "ts": 123918373,
-          "source": "user"
-        }
-      ]
-    }));
-
-    const resp = createInContextPost(context)
-    resp.then((bres) => {
-        return   res.status(HTTP_SUCCESS).json(JSON.parse(bres))
-    }).catch((err) => {
-        console.log(err.toString())
-        return res.status(HTTP_INTERNAL_ERROR).json(err.toString())
-    })
+    try {
+        const resp = createInContextPost(req.body)
+        resp.then((bres) => {
+            return   res.status(HTTP_SUCCESS).json(JSON.parse(bres))
+        }).catch((err) => {
+            console.log(err.toString())
+            return res.status(HTTP_INTERNAL_ERROR).json(JSON.stringify({"error": err.toString()}))
+        })
+    }
+    catch(e){
+        return res.status(HTTP_INTERNAL_ERROR).json({"error": e.toString()})
+    }
 })
 
 backendRouter.post('/complete', async function(req, res) {
-  // const userId:string = req.body.user
-  // const postId:string = req.body.postId
-  const prefix:string = req.body.prefix
-  const payload = JSON.parse(JSON.stringify({"prefix" : prefix }));
-  const resp = complete(payload)
+
+  const resp = complete(req.body)
 
   resp.then((backendResonse) => {
     return   res.status(HTTP_SUCCESS).json(JSON.parse(backendResonse))
   }).catch((err) => {
     console.log(err.toString())
-    return res.status(HTTP_INTERNAL_ERROR).json({"error": err.toString()})
+    return res.status(HTTP_INTERNAL_ERROR).json(JSON.stringify({"error": err.toString()}))
   })
 })
--- src/service/postGeneraton.ts
+++ src/service/postGeneraton.ts
@@ -1,17 +1,46 @@
-import { Pool } from 'pg'
+import express from 'express'
 import {BACKEND_HOST, AUTOCOMPLETE_PATH, IN_CONTEXT_PATH} from "./config";
 import {post} from "./httpService";
 
-export async function createInContextPost(context: JSON): Promise<string>{
+export async function createInContextPost(data: any): Promise<string>{
+    const temperature: number = data.temperature
+    const postId: number = data.postId
+    const turns: number = data.turns
+
+    const dummmy_payload = JSON.parse(JSON.stringify({
+        "context" : [
+            {
+                "input": "Hi Dude",
+                "output": "Hi man, what are you up to ?",
+                "ts": 123918273,
+                "source": "lm"
+            },
+            {
+                "input": "Not much, working mostly.",
+                "output": "Thats too bad im in Malaga !",
+                "ts": 123918373,
+                "source": "user"
+            }
+        ],
+        "temperature": temperature
+    }));
 
     const url = BACKEND_HOST + IN_CONTEXT_PATH
 
-    return post(url, context)
+    return post(url, dummmy_payload)
 }
 
-export async function complete(prefix: JSON): Promise<string>{
+export async function complete(data: any): Promise<string>{
 
     const url = BACKEND_HOST + AUTOCOMPLETE_PATH
 
-    return post(url, prefix)
+    return post(url, data)
+}
+
+function validate_body(payload:any, reqParams:Array<string>): void {
+    for (const rp of reqParams){
+        if (!payload.hasOwnProperty(rp)) {
+           throw Error(`required but missing param '${rp}'`)
+        }
+    }
 }
